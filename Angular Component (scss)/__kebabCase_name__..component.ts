e80807793f720e8a8d5263cae3942da20423c937
Duy Nguyen <duy.nguyen@prolifiq.com>
2017-04-10 14:49:55 -0700

    Updated syntax in example templates to match changes from blueprint project (issue #13)

---
+++ Angular Component (scss)/__kebabCase_name__..component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-prefix-{{kebabCase name}}',
+  templateUrl: './{{kebabCase name}}.component.html',
+  styleUrls: ['./{{kebabCase name}}.component.scss'],
+})
+export class {{pascalCase name}}Component {
+
+}
